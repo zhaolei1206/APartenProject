752c40a92a770f4655faf3c402350f10585ae5df
liulong2 <l893043230@163.com>
2020-02-12 15:24:04 +0800

    新增数据

---
+++ APartenProject/UserProvdier01/src/main/java/com/offcn/AppStartApplication.java
@@ -0,0 +1,13 @@
+package com.offcn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class AppStartApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(AppStartApplication.class,args);
+    }
+}
---
+++ APartenProject/UserProvdier01/src/main/java/com/offcn/po/User.java
@@ -0,0 +1,24 @@
+package com.offcn.po;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+    @Id
+    @GeneratedValue
+    private Long id;
+    @Column(name="name",nullable = true,length = 200)
+    private String name;
+    @Column(name="age",nullable = true,length = 4)
+    private Integer age;
+}
